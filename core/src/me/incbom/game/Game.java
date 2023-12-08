12772f56d0f9d041bcaadadd33f616a1ce4e1922
Incbom <99501939+IncbomDev@users.noreply.github.com>
2023-12-08 12:49:15 -0500

    stuff

---
+++ core/src/me/incbom/game/Game.java
@@ -0,0 +1,30 @@
+package me.incbom.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class Game extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture img;
+
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+	}
+
+	@Override
+	public void render () {
+		ScreenUtils.clear(1, 0, 0, 1);
+		batch.begin();
+		batch.end();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+		img.dispose();
+	}
+}
